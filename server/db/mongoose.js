89fd3409771b258a081fdb22a3978972b844fa82
MEOW <mdigitalcomm@gmail.com>
2018-12-20 16:47:28 -0500

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).then(() => {
   console.log('Connected to Database');
 }).catch(err => {
   console.log("Not connected to Database", err);
